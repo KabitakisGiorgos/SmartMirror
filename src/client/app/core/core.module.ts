2d7bf0c9f4f7fe98772a866cd6cec8cf00e50829
GKampitakis <csd3142@gmail.com>
2019-03-08 11:12:47 +0200

    Removed unnecessary components

--- src/client/app/core/core.module.ts
+++ src/client/app/core/core.module.ts
@@ -1,18 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
-import { HomeComponent } from './home/home.component';
-import { AboutComponent } from './about/about.component';
+import { HomeComponent } from './home/home.component';;
 import { CoreRoutingModule } from './core.routes.module';
-import { NavbarComponent } from './navbar/navbar.component';
-import { FooterComponent } from './footer/footer.component';
-
 @NgModule({
   declarations: [
-    HomeComponent,
-    AboutComponent,
-    NavbarComponent,
-    FooterComponent
+    HomeComponent
   ],
   imports: [
     CommonModule,
@@ -20,8 +13,6 @@ import { FooterComponent } from './footer/footer.component';
   ],
   exports: [
     CoreRoutingModule,
-    NavbarComponent,
-    FooterComponent
   ]
 })
 export class CoreModule { }
