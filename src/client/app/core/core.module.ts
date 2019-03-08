94d86589fae3f58ad685ec9b64fd4d5fcace2253
GKampitakis <csd3142@gmail.com>
2019-03-08 18:43:17 +0200

    updated the angular and testing the carousel

--- src/client/app/core/core.module.ts
+++ src/client/app/core/core.module.ts
@@ -1,15 +1,16 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
-import { HomeComponent } from './home/home.component';;
+import { HomeComponent } from './home/home.component';
 import { CoreRoutingModule } from './core.routes.module';
+import { SlickCarouselModule } from 'ngx-slick-carousel';
 @NgModule({
   declarations: [
     HomeComponent
   ],
   imports: [
     CommonModule,
-    CoreRoutingModule
+    CoreRoutingModule,
+    SlickCarouselModule
   ],
   exports: [
     CoreRoutingModule,
--- src/client/app/core/home/home.component.ts
+++ src/client/app/core/home/home.component.ts
@@ -4,4 +4,57 @@ import { Component } from '@angular/core';
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
-export class HomeComponent { }
+export class HomeComponent {
+  slides: any[] = [];
+  slideConfig: any;
+  constructor() {
+    this.slides = [
+      {
+        title: 'http://placehold.it/350x150/000000',
+        name: 'test1'
+      },
+      {
+        title: 'http://placehold.it/350x150/111111',
+        name: 'test2'
+      },
+      {
+        title: 'http://placehold.it/350x150/333333',
+        name: 'test3'
+      },
+      {
+        title: 'http://placehold.it/350x150/666666',
+        name: 'test4'
+      }
+    ];
+    this.slideConfig = {
+      'slidesToShow': 2,
+      'slidesToScroll': 2,
+      'accessibility': true,
+      'useTransform': false//TODO: dont change it is for flickering
+    };
+  }
+
+  addSlide() {
+    this.slides.push({ img: 'http://placehold.it/350x150/777777' })
+  }
+
+  removeSlide() {
+    this.slides.length = this.slides.length - 1;
+  }
+
+  slickInit(e) {
+    console.log('slick initialized');
+  }
+
+  breakpoint(e) {
+    console.log('breakpoint');
+  }
+
+  afterChange(e) {
+    console.log('afterChange');
+  }
+
+  beforeChange(e) {
+    console.log('beforeChange');
+  }
+}
