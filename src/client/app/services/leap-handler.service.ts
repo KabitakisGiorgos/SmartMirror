3a61b77de750480ea2675de8ff2b9dd39c3e30a9
GKampitakis <csd3142@gmail.com>
2019-03-27 10:26:18 +0200

    Fixed the leap with the correct library

--- src/client/app/services/leap-handler.service.ts
+++ src/client/app/services/leap-handler.service.ts
@@ -16,11 +16,48 @@ export class LeapHandlerService {
 
   constructor() {
     this.cursor = new Cursor();
+
     // this.debug = debugMode['LeapHandlerService'];
     this.init();
   }
 
   init() {
     this.controller = new Leap.Controller();
+
+    this.controller.loop((frame) => {
+      // NOOP
+      var appWidth = $(window).width();
+      var appHeight = $(window).height();
+
+      var iBox = frame.interactionBox;
+      var pointable = frame.hands[0];
+
+      if (pointable != undefined) {
+        this.cursor.SetOrientation(pointable.type);
+        this.cursor.Show();
+
+        this.leapTimer = 0;
+        var leapPoint = pointable.stabilizedPalmPosition;
+        var normalizedPoint = iBox.normalizePoint(leapPoint, true);
+
+        var appX = normalizedPoint[0] * appWidth;
+        var appY = (1 - normalizedPoint[1]) * appHeight;
+        this.cursorOn = true;
+        this.cursor.Move(appY, appX);
+      } else {
+        if (!this.cursorOn)
+          return;
+
+        if (this.leapLastTimeNoHand > 0)
+          this.leapTimer += performance.now() - this.leapLastTimeNoHand;
+        this.leapLastTimeNoHand = performance.now();
+
+        if (this.leapTimer > 1500) {//FIXME: add it to  a config
+          this.cursor.Hide();
+          this.cursorOn = false;
+        }
+      }
+
+    });
   }
 }
