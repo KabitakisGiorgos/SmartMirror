8881afbb664fbbed04e79cc3dabfa16273a09c76
GKampitakis <csd3142@gmail.com>
2019-04-18 02:12:31 +0300

    Started Implementing the media page

--- src/client/app/pages/media/media.component.ts
+++ src/client/app/pages/media/media.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-media',
@@ -6,11 +6,93 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./media.component.scss']
 })
 export class MediaComponent implements OnInit {
+  @ViewChild('slickModal') carousel: any;
+  slides = [
+    { img: 'http://placehold.it/350x150/000000' },
+    { img: 'http://placehold.it/350x150/111111' },
+    { img: 'http://placehold.it/350x150/333333' },
+    { img: 'http://placehold.it/350x150/666666' },
+    { img: 'http://placehold.it/350x150/000000' },
+    { img: 'http://placehold.it/350x150/111111' },
+    { img: 'http://placehold.it/350x150/333333' },
+    { img: 'http://placehold.it/350x150/666666' },
+    { img: 'http://placehold.it/350x150/000000' }
+  ];
 
+  favorites = [
+    {
+      title: 'Post Malone - rockstar ft. 21 Savage',
+      image: '../../../assets/images/post.png',
+      repeats: '12',
+      duration: '4:01',
+    },
+    {
+      title: 'Iron Maiden - Alexander The Great',
+      image: '../../../assets/images/alexander.png',
+      repeats: '22',
+      duration: '8:34',
+    },
+    {
+      title: 'Iron maiden - the legacy',
+      image: '../../../assets/images/legacy.png',
+      repeats: '11',
+      duration: '9:23',
+    },
+    {
+      title: 'Metallica - Sad But True',
+      image: '../../../assets/images/sad.png',
+      repeats: '9',
+      duration: '5:28',
+    },
+    {
+      title: 'Metallica - Sad But True',
+      image: '../../../assets/images/sad.png',
+      repeats: '9',
+      duration: '5:28',
+    },
+    {
+      title: 'Metallica - Sad But True',
+      image: '../../../assets/images/sad.png',
+      repeats: '9',
+      duration: '5:28',
+    },
+    {
+      title: 'Metallica - Sad But True',
+      image: '../../../assets/images/sad.png',
+      repeats: '9',
+      duration: '5:28',
+    },
+    {
+      title: 'Metallica - Sad But True',
+      image: '../../../assets/images/sad.png',
+      repeats: '9',
+      duration: '5:28',
+    },
+    {
+      title: 'Metallica - Sad But True',
+      image: '../../../assets/images/sad.png',
+      repeats: '9',
+      duration: '5:28',
+    }
+  ];
+  slideConfig = {
+    slidesToShow: 6,
+    slidesToScroll: 6,
+    arrows: false,
+    infinite: false,
+  };
   constructor() { }
 
   ngOnInit() {
 
+  }
+
+  next() {
+    this.carousel.slickNext();
+  }
+
+  previous() {
+    this.carousel.slickPrev();
   }
 
 }
